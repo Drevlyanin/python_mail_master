7050db4e6c1efaba775a019b192e4db614ca1a7f
Drevlyanin <121987689+Drevlyanin@users.noreply.github.com>
2023-03-18 05:15:33 +0200

    Create MailMaster.py

---
+++ MailMaster.py
@@ -0,0 +1,58 @@
+import imaplib
+import email
+import os
+import telebot
+import time
+import shutil
+
+# set up your bot using the bot token provided by BotFather
+bot = telebot.TeleBot('6003923785:AAFqVJaZnBSNoTPLNUzXZZqb_mQ6Z1kEv5o')
+
+# set up your email credentials
+mail = imaplib.IMAP4_SSL('imap.gmail.com')
+mail.login('GeorgyLabartkava@gmail.com', 'yrgoitwpbcdiyayd')
+mail.select('inbox')
+
+# set up a list to keep track of processed message IDs
+processed_ids = []
+
+# function to check for new mail and send a message to the bot
+def check_mail():
+    global processed_ids
+
+    mail.select('inbox')
+    _, search_data = mail.search(None, 'UNSEEN')
+    mail_ids = search_data[0].split()
+
+    for id in mail_ids:
+        if id in processed_ids:
+            continue
+
+        _, data = mail.fetch(id, '(RFC822)')
+        _, b = data[0]
+        msg = email.message_from_bytes(b)
+
+        # extract the sender and subject of the email
+        sender = msg['From']
+        subject = ''.join([text.encode(encoding or 'utf-8').decode('utf-8') for text, encoding in email.header.decode_header(msg['Subject'])]) if msg['Subject'] else ''
+
+
+
+        # send a message to the bot with the email subject
+        print("sender:", sender)
+        print("subject:", subject)
+        if subject:
+            bot.send_message(chat_id='581851299', text="Hello, you've got mail from {} with the subject '{}'!".format(sender, subject))
+        else:
+            bot.send_message(chat_id='581851299', text="Hello, you've got mail from {} with no subject.".format(sender))
+
+        # add the ID of the processed message to the list
+        processed_ids.append(id)
+
+# run the check_mail function every 10 minutes
+while True:
+    check_mail()
+    time.sleep(6)
+
+# remove the 'temp' directory and its contents
+shutil.rmtree('temp')
